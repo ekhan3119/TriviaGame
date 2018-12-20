2b6dac652505ad8fca92a675bbe35b658eaddd56
ekhan3119 <ekc3119@gmail.com>
2018-12-20 18:49:08 -0500

    added html and javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+//create variable for question and answer.
+var wordsList = ["jerome", "neena", "darion", "lou", "greg", "jordan",
+"jasmine", "stephen", "jacob", "adam", "rui", "luis"];
+
+// user selected answer will be held here.
+var chosenAnswers = "";
+
+// This will break the solution into individual letters to be stored in array.
+var answerInChosen = [];
+
+// This will be the number of blanks we show based on the solution.
+var numBlanks = 0;
+
+// Holds a mix of blank and solved letters (ex: 'n, _ _, n, _').
+var blanksAndSuccesses = [];
+
+// Holds all of the wrong guesses.
+var wrongGuesses = [];
+
+// Holds the letters guessed
+var letterGuessed = "";
+
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
+//
+function buildQuiz(){}
+
+function showResult(){}
+
+
+
+submitButton.click
+
+
+
+
+
+//$("#start")on('click', function()   newGame();});
+function newGame(){
+    
+}
